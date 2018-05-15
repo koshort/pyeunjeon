19d4e568192c15205fec44e6ad6d9597c46da3fc
Jung <iam@nyanye.com>
2018-05-15 09:38:56 +0900

    Add version information

--- eunjeon/__init__.py
+++ eunjeon/__init__.py
@@ -1,3 +1,15 @@
 from __future__ import absolute_import
 
+import pkg_resources
+
+__title__ = 'eunjeon'
+__author__ = 'nyanye'
+__license__ = 'GPL v3'
+__copyright__ = 'Copyright 2018 Nyanye'
+
+try:
+    __version__ = pkg_resources.get_distribution('eunjeon').version
+except pkg_resources.DistributionNotFound:
+    __version__ = "dev"
+
 from eunjeon._mecab import Mecab
