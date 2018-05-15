b40dc9672ab072a92d487b47aa091dd0bfbf4733
Jung <iam@nyanye.com>
2018-05-15 09:08:13 +0900

    Fix initialization

--- eunjeon/__init__.py
+++ eunjeon/__init__.py
@@ -1,6 +1,3 @@
 from __future__ import absolute_import
 
-try:
-    from eunjeon._mecab import Mecab
-except:  # ImportError and ModuleNotFoundError from python3.5+ is expected.
-    print("Install either mecab-python or mecab-python-windows in order to use mecab tagging. you can easily install one of them with :pip install mecab-python")
+from eunjeon._mecab import Mecab
