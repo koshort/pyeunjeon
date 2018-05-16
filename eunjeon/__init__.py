f3b2df04fae2042bae2be84ef417561fa59308cb
Jung <iam@nyanye.com>
2018-05-16 17:32:40 +0900

    Add doc and installpath

--- eunjeon/__init__.py
+++ eunjeon/__init__.py
@@ -1,6 +1,15 @@
+"""
+Eunjeon
+=======
+
+Eunjeon is a Korean morphological analysis project made on top of mecab.
+This is python implementation of eunjeon with stand-alone installation functionality.
+"""
+
 from __future__ import absolute_import
 
 import pkg_resources
+import os
 
 __title__ = 'eunjeon'
 __author__ = 'nyanye'
@@ -11,5 +20,7 @@ try:
     __version__ = pkg_resources.get_distribution('eunjeon').version
 except pkg_resources.DistributionNotFound:
     __version__ = "dev"
+
+installpath = os.path.dirname(os.path.realpath(__file__))
 
 from eunjeon._mecab import Mecab
